2b0fbdfbc418deae5c4da5161755ac8020bcf865
Andreu_Elias_Puigvert_Gomez <145295604+1702901@users.noreply.github.com>
2024-06-03 02:26:18 +0200

    Delete funcionAuxiliar.h

--- funcionAuxiliar.h
+++
@@ -1,7 +0,0 @@
-#include "struct.h"
-
-int arrayLenghFigura(int tipusFigura);
-void numToBinary(int num, bool array[]);
-void bynaryToFigure(bool* num, int* array,const int color);
-void arrayToFigure(int tipusFigura, int array[],int& tamany, const int color, int& lenghLine);
-void intercambia(int& num1,int& num2);
